7c5af54edfb4962313db79f497f6f320a9b3869c
Nobunaga-2001 <allenjoy.signo@tup.edu.ph>
2024-10-19 21:56:43 +0800

    s

---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mainpage = () => {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <p>Welcome to the main page of the app!</p>
+        </div>
+    );
+};
+
+export default Mainpage;
