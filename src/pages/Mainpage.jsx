2e1971304a727d34d25bc8289182ea82edc40313
Nobunaga-2001 <allenjoy.signo@tup.edu.ph>
2024-10-20 01:49:44 +0800

    dasd

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Mainpage from './Mainpage'; // Your main page component
-import AdminPage from './AdminPage'; // Your admin page component
+import Admin from './Admin'; // Your admin page component
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Mainpage />} />
-                <Route path="/admin" element={<AdminPage />} />
+                <Route path="/admin" element={<Admin />} />
             </Routes>
         </Router>
     );
