f2c8f716e11d67136fbb08eeadaa82a6bc173ada
Nobunaga-2001 <allenjoy.signo@tup.edu.ph>
2024-10-19 21:58:48 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Mainpage from './pages/Mainpage';
-import Admin from './pages/Admin';
+import Mainpage from './pages/Mainpage'; // Import Mainpage
+import Admin from './pages/Admin';       // Import Admin
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Mainpage />} />
-                <Route path="/admin" element={<Admin />} />
-                {/* Add other routes here if needed */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My React App</h1>
+        </header>
+        <Routes>
+          {/* Route for Mainpage */}
+          <Route path="/" element={<Mainpage />} />
+          {/* Route for Admin */}
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
